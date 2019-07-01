4b655d1886c3a66caf6ce76f4dc3263c882dcd7e
tommy-park <tommy.park@key-inside.com>
2019-07-01 14:43:01 +0900

    page view add margin bottom

--- client/src/components/Styled/View.js
+++ client/src/components/Styled/View.js
@@ -13,6 +13,7 @@ const styles = theme => {
     fullwidth: {
       width: '100%',
       marginTop: 105,
+      marginBottom: 100,
       backgroundColor: dark ? 'rgb(36, 32, 54)' : '#f0f5f9'
     },
     display: {
